792810d15995b165a236ba90f3954f0d83278cd2
Abhijeet61 <129062674+Abhijeet61@users.noreply.github.com>
2023-04-02 18:53:16 +0530

    Add files via upload

---
+++ footer.inc.php
@@ -0,0 +1,60 @@
+<!-- <html>
+   <body>
+      <div class="clearfix"></div>
+         <footer class="site-footer">
+            <div class="footer-inner bg-white">
+               <div class="row">
+                  <div class="col-sm-6">
+                     Copyright &copy; <b>JStreams Solution Pvt. Ltd.</b>
+                  </div>
+                  <div class="col-sm-6 text-right">
+                     Designed by <a href="#"><b>Team 2</b></a>
+                  </div>
+               </div>
+            </div>
+         </footer>
+      </div>
+      <script src="assets/js/vendor/jquery-2.1.4.min.js" type="text/javascript"></script>
+      <script src="assets/js/popper.min.js" type="text/javascript"></script>
+      <script src="assets/js/plugins.js" type="text/javascript"></script>
+      <script src="assets/js/main.js" type="text/javascript"></script>
+   </body>
+</html> -->
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>footer</title>
+
+<link rel="stylesheet" href="assets/css/footer.css" />
+
+</head>
+
+<body>
+
+   <footer class="footer">
+
+        <div class="footer-inner bg-white">
+
+           <div class="row">
+
+              <div class="col-sm-6">
+
+                 Copyright &copy; JStreams Solution Pvt. Ltd. <b>Designed by <a href="#">Team 2</a></b>
+
+              </div>
+
+           </div>
+
+        </div>
+
+     </footer>
+
+</body>
+
+</html>
